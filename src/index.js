4662810df58fbc1e09a6c467d28f47597f74b6cd
celineWan1210 <wanyingxuan1210@gmail.com>
2026-02-12 21:53:05 +0800

    Able to create object and save as JSON

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello");
+import { v4 as uuidv4 } from 'uuid';
+
+// create default project and save as JSON
+function createDefaultProject() {
+    const list = [];
+
+    const item1 = createTodoItem("Homework");
+    const item2 = createTodoItem("Reading");
+    saveAsList(item1, list);
+    saveAsList(item2, list);
+
+    const jsonString = JSON.stringify(list);
+    console.log(jsonString);
+}
+
+// create to-do-list item and append to list
+class ToDoItem {
+    constructor(title) {
+        this.id = uuidv4(); 
+        this.title = title;
+    }
+}
+
+// create new Item
+function createTodoItem(title) {
+    const item = new ToDoItem(title);
+    return item;
+}
+
+// save todoItem to a list
+function saveAsList(item, list) {
+    list.push(item);
+}
+
+createDefaultProject();
