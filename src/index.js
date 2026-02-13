1631bbee1bc22451f92b148971d1d45b1f35fc16
celineWan1210 <wanyingxuan1210@gmail.com>
2026-02-13 11:58:38 +0800

    Seperate functions into different function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
-import { v4 as uuidv4 } from 'uuid';
+import localStorage from "./functions/localStorage.js"
 
 // create default project and save as JSON
-function createDefaultProject() {
-    const list = [];
-
-    const item1 = createTodoItem("Homework");
-    const item2 = createTodoItem("Reading");
-    saveAsList(item1, list);
-    saveAsList(item2, list);
-
-    const jsonString = JSON.stringify(list);
-    console.log(jsonString);
+function showDefaultProject() {
+    localStorage();
 }
 
-// create to-do-list item and append to list
-class ToDoItem {
-    constructor(title) {
-        this.id = uuidv4(); 
-        this.title = title;
-    }
-}
-
-// create new Item
-function createTodoItem(title) {
-    const item = new ToDoItem(title);
-    return item;
-}
-
-// save todoItem to a list
-function saveAsList(item, list) {
-    list.push(item);
-}
-
-createDefaultProject();
+showDefaultProject();
